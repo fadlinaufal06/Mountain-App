7c5e17d8efcbee09b861fb56b4008cf758de550f
fadlinaufal06 <18219043@std.stei.itb.ac.id>
2022-05-28 20:54:03 +0700

    tes tes

---
+++ app/src/main/java/com/bangkit/mountainapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bangkit.mountainapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //halo
+        // halo gais
+    }
+}
