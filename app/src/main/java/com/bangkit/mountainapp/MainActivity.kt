31b101b9b324e2124e4ff610cbbaaa0eb6b6a85a
yendrapriambada <yendrapriambada1234@gmail.com>
2022-06-04 11:51:31 +0700

    Delete: HomeActivity

--- app/src/main/java/com/bangkit/mountainapp/MainActivity.kt
+++ app/src/main/java/com/bangkit/mountainapp/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.bangkit.mountainapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bangkit.mountainapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //halo
-        // halo gais
 
-        // test 123
-        // test lagi
-        //tes liat home activity nya
-        startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_feeds,
+                R.id.navigation_favorite,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
