939b803734ab3de769704194c7a4d2f535c3b0fe
fadlinaufal06 <18219043@std.stei.itb.ac.id>
2022-06-08 14:51:42 +0700

    add home header

--- app/src/main/java/com/bangkit/mountainapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/mountainapp/ui/home/HomeFragment.kt
@@ -18,6 +18,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
+
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
