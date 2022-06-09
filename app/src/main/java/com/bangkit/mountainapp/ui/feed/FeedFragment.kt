2155246aef4f0284fbd2fbfd29563aefa3c0ec01
fadlinaufal06 <18219043@std.stei.itb.ac.id>
2022-06-09 10:00:40 +0700

    add layout profile

--- app/src/main/java/com/bangkit/mountainapp/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/bangkit/mountainapp/ui/feed/FeedFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bangkit.mountainapp.databinding.FragmentFeedsBinding
 
 class FeedFragment : Fragment() {
-
     private var _binding: FragmentFeedsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -28,12 +28,9 @@ class FeedFragment : Fragment() {
         _binding = FragmentFeedsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textFeed
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
